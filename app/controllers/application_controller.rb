6d38f9fa5f798239993badc99ea988592dcefc83
elizachen <elizachen34@gmail.com>
2017-09-15 13:36:27 +0800

    Step5. Add check_mfa in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_filter :check_mfa
+
+  def current_user
+    @current_user = User.find_or_create_by(name: 'elizachen', email: 'elizachen34@gmail.com')
+  end
+
+  private
+  def check_mfa
+     if !(user_mfa_session = UserMfaSession.find) && (user_mfa_session ? user_mfa_session.record == current_user : !user_mfa_session)
+      redirect_to new_user_mfa_session_url
+    end
+  end
 end
